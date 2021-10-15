687eb6c351bfdf937f13d3fc25bc28169f43fb11
Jesus Bossa <x@jesusbossa.dev>
2021-10-14 20:27:32 -0500

    chore: add test for toggle func

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -2,11 +2,21 @@
 pragma solidity ^0.8.6;
 
 contract TaskContract {
-    uint public taskCounter = 0;
+    uint256 public taskCounter = 0;
 
-    constructor () {
+    constructor() {
         createTask("Mi primer tarea de ejemplo", "Mi primera descripcion");
     }
+
+    event TaskCreated(
+        uint256 id,
+        string title,
+        string description,
+        bool done,
+        uint256 createdAt
+    );
+
+    event TaskToggleDone(uint256 id, bool done);
 
     struct Task {
         uint256 id;
@@ -21,13 +31,27 @@ contract TaskContract {
     function createTask(string memory _title, string memory _description)
         public
     {
-        task[taskCounter] = Task(taskCounter, _title, _description, false, block.timestamp);
+        uint256 timeStamp = block.timestamp;
+        task[taskCounter] = Task(
+            taskCounter,
+            _title,
+            _description,
+            false,
+            timeStamp
+        );
+
+        emit TaskCreated(taskCounter, _title, _description, false, timeStamp);
+
         taskCounter++;
     }
 
-    function toggleDone(uint _id) public {
+    function toggleDone(uint256 _id) public {
         Task memory _task = task[_id];
-        _task.done = !_task.done;
+        bool toggled = !_task.done;
+
+        _task.done = toggled;
         task[_id] = _task;
+
+        emit TaskToggleDone(_id, toggled);
     }
 }
--- test/TaskContract.test.js
+++ test/TaskContract.test.js
@@ -1,14 +1,14 @@
 const TaskContract = artifacts.require("TaskContract");
 
 contract("TaskContract", () => {
-  let taskc;
+  let taskRef;
 
   before(async () => {
-    taskc = await TaskContract.deployed();
+    taskRef = await TaskContract.deployed();
   });
 
   it("migrate deployed successfully", async () => {
-    const address = taskc.address;
+    const address = taskRef.address;
 
     assert.notEqual(address, null);
     assert.notEqual(address, undefined);
@@ -17,10 +17,35 @@ contract("TaskContract", () => {
   });
 
   it("get task list", async () => {
-    const counter = await taskc.taskCounter();
-    const actualTask = await taskc.task(counter.toNumber() - 1);
+    const counter = await taskRef.taskCounter();
+    const actualTask = await taskRef.task(counter.toNumber() - 1);
 
     assert.typeOf(counter.toNumber(), "number");
-    assert.equal(actualTask.id.toNumber(), counter - 1);
+    assert.equal(actualTask.title, "Mi primer tarea de ejemplo");
+    assert.equal(actualTask.description, "Mi primera descripcion");
+    assert.equal(actualTask.done, false);
+    assert.equal(counter.toNumber(), 1);
+  });
+
+  it("created new task successfully", async () => {
+    const result = await taskRef.createTask("test task", "description two");
+    const taskEvent = result.logs[0].args;
+    const counter = await taskRef.taskCounter();
+
+    assert.equal(taskEvent.id.toNumber(), counter.toNumber() - 1);
+    assert.equal(taskEvent.title, "test task");
+    assert.equal(taskEvent.description, "description two");
+    assert.equal(taskEvent.done, false);
+  });
+
+  it("task toggle done", async () => {
+    const taskId = 0;
+    const result = await taskRef.toggleDone(taskId);
+    const actualTask = await taskRef.task(taskId);
+    const taskEvent = result.logs[0].args;
+
+    assert.equal(taskEvent.done, true);
+    assert.equal(taskEvent.done, actualTask.done);
+    assert.equal(taskEvent.id.toNumber(), actualTask.id.toNumber());
   });
 });
