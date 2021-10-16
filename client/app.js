966aa8212b2fa772807949cef5748311b1fa68de
Jesus Bossa <x@jesusbossa.dev>
2021-10-15 21:12:20 -0500

    feat: connect wallets

--- client/app.js
+++ client/app.js
@@ -1,15 +1,19 @@
 App = {
   web3Provider: undefined,
+  web3: undefined,
 
   init: () => {
     console.log("init");
     App.loadEthereum();
   },
 
-  loadEthereum: () => {
+  loadEthereum: async () => {
     if (window.ethereum) {
       console.log("ethereum exist");
       App.web3Provider = window.ethereum;
+      await App.web3Provider.request({ method: "eth_requestAccounts" });
+    } else if (window.web3) {
+      web3 = new Web3(window.web3.currentProvider);
     } else {
       console.log("Please install a ethereum wallet in your browser");
     }
