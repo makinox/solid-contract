0941901a2066ef3530f39b13a97017a4b60b4466
Jesus Bossa <x@jesusbossa.dev>
2021-10-18 15:41:18 -0500

    chore: react connect accounts

--- src/page/Index.tsx
+++ src/page/Index.tsx
@@ -3,13 +3,33 @@ import TaskCard from "../components/TaskCard/TaskCard";
 import Navbar from "../components/navbar/navbar";
 
 import { MainSectionStyles } from "./Index.styles";
+import { useEffect, useState } from "react";
 
 export default function Index() {
+  const [provider, setProvider] = useState();
+  const [account, setAccount] = useState("");
+
+  async function loadEthereum() {
+    if (window["ethereum"]) {
+      console.log("ethereum exist");
+      const web3Provider = window["ethereum"];
+      setProvider(web3Provider);
+      const account = await web3Provider.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(account[0]);
+    }
+  }
+
+  useEffect(() => {
+    loadEthereum();
+  }, []);
+
   return (
     <>
       <Navbar />
       <section className={`flex justify-center ${MainSectionStyles}`}>
-        <InfoCard title="Wallet" description="" isDark={false} />
+        <InfoCard title="Wallet" description={account} isDark={false} />
       </section>
       <section className="flex justify-center flex-wrap">
         <TaskCard title="Tarea 1" description="Description 1" isDark={false} />
