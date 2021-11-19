c610c1471518f3fb9a27bb41b105171633bf6ebf
Jesus Bossa <x@jesusbossa.dev>
2021-11-18 21:34:04 -0500

    chore: navbar styled

--- client/ui.js
+++ client/ui.js
@@ -1,10 +1,19 @@
-const taskForm = document.querySelector("#taskForm");
+window.addEventListener("DOMContentLoaded", (event) => {
+  const taskForm = document.querySelector("#taskForm");
 
-taskForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  const title = taskForm["taskForm-title"].value;
-  const description = taskForm["taskForm-description"].value;
+    const title = taskForm["taskForm-title"].value;
+    const description = taskForm["taskForm-description"].value;
 
-  App.createTask(title, description);
+    App.createTask(title, description);
+  });
+
+  // Apliying styles
+  const navbar = document.querySelector("nav");
+  navbar.className = window.TopBar_styles({});
+
+  const section = document.querySelector("nav section");
+  section.className = `flex items-center justify-between ${window.FluidContainer_styles()}`;
 });
